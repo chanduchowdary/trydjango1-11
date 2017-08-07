dadfc0fd9256435a3c522137f5013870c3b04540
Chandra Sekhar <MPROGA1\chandra.sekhar@a1-m-d02.mproga1.com>
2017-08-08 04:56:19 +0530

    Initial commit

---
+++ restaurants/views.py
@@ -0,0 +1,34 @@
+from django.db.models import Q
+from django.http import HttpResponse
+from django.shortcuts import render
+from django.views import View
+from django.views.generic import TemplateView,ListView
+
+from .models import RestaurantLocation
+
+def restaurant_listview(request):
+	template_name='restaurants/restaurants_list.html'
+	queryset = RestaurantLocation.objects.all()
+	context = {
+				"object_list":queryset
+	}
+	return render(request,template_name,context)
+
+class RestaurantListView(ListView):
+	queryset = RestaurantLocation.objects.all()
+	template_name='restaurants/restaurants_list.html'
+
+class SearchRestaurantListView(ListView):
+	template_name='restaurants/restaurants_list.html'
+
+	def get_query_set(self):
+		print(self.kwargs)
+		slug = self.kwargs.get("slug")
+		if slug:
+			queryset = RestaurantLocation.objects.filter(
+				Q(category__iexact= slug)
+				)
+		else :
+			queryset = RestaurantLocation.objects.none()
+		return queryset
+
