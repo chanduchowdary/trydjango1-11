5dd5e5f527f9f769d8bd3c16beecbd63c2ed9da4
chanduchowdary <sekharchandu474@gmail.com>
2017-08-09 04:03:35 +0530

    Updated

--- restaurants/views.py
+++ restaurants/views.py
@@ -1,34 +1,45 @@
 from django.db.models import Q
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views import View
-from django.views.generic import TemplateView,ListView
+from django.views.generic import TemplateView, ListView, DetailView
 
 from .models import RestaurantLocation
 
-def restaurant_listview(request):
-	template_name='restaurants/restaurants_list.html'
-	queryset = RestaurantLocation.objects.all()
-	context = {
-				"object_list":queryset
-	}
-	return render(request,template_name,context)
+def restaurant_listview(request,):
+    template_name = 'restaurants/restaurants_list.html'
+    queryset = RestaurantLocation.objects.all()
+    context = {
+        "object_list": queryset
+    }
+    return render(request, template_name, context)
+
+
+def restaurant_detailview(request, slug):
+    template_name = 'restaurants/restaurantlocation_detail.html'
+    obj = RestaurantLocation.objects.get(slug=slug)
+    context = {
+        "object": obj
+    }
+    return render(request, template_name, context)
 
 class RestaurantListView(ListView):
-	queryset = RestaurantLocation.objects.all()
-	template_name='restaurants/restaurants_list.html'
+    def get_queryset(self):
+        slug = self.kwargs.get("slug")
+        if slug:
+            queryset = RestaurantLocation.objects.filter(
+                    Q(category__iexact=slug) |
+                    Q(category__icontains=slug)
+                )
+        else:
+            queryset = RestaurantLocation.objects.all()
+        return queryset
 
-class SearchRestaurantListView(ListView):
-	template_name='restaurants/restaurants_list.html'
 
-	def get_query_set(self):
-		print(self.kwargs)
-		slug = self.kwargs.get("slug")
-		if slug:
-			queryset = RestaurantLocation.objects.filter(
-				Q(category__iexact= slug)
-				)
-		else :
-			queryset = RestaurantLocation.objects.none()
-		return queryset
-
+class RestaurantDetailView(DetailView):
+    queryset = RestaurantLocation.objects.all() #.filter(category__iexact='asian')
+    
+    # def get_object(self, *args, **kwargs):
+    #     rest_id = self.kwargs.get('rest_id')
+    #     obj = get_object_or_404(RestaurantLocation, id=rest_id) # pk = rest_id
+#     return obj
